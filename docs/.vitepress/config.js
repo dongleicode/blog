da74ceefa45fffc55787b1210ab45a5deab6c020
dongleicode <mhwgodl@163.com>
2023-01-28 11:03:01 +0800

    样式修改

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,46 +5,55 @@ export default {
         siteTitle: 'JINRUI',
         // logo: '/favicon.ico',
         nav: [
-            { text: 'github', link: 'https://github.com/dongleicode' },
-            // {
-            //     text: '联系',
-            //     items: [
-            //         { text: 'email', link: 'https://dongleicode.github.io/blog' },
-            //     ]
-            // }
+            {
+                text: 'contact',
+                items: [
+                    { text: 'github', link: 'https://github.com/dongleicode' },
+                    { 
+                        text: 'email：mhwgodl@163.com' 
+                    }
+                ]
+            }
         ],
         sidebar: {
             '/': [
-              {
-                text: 'Vue',
-                collapsible: true,
-                items: [
-                  {
-                    text: 'vue2和vue3核心原理差异',
-                    link: '../vue/core'
-                  }
-                ]
-              },
-              {
-                text: 'React',
-                collapsible: true,
-                items: [
-                  {
-                    text: 'React核心',
-                    link: '../react/',
-                  }
-                ]
-              },
-              {
-                text: '前端架构',
-                collapsible: true,
-                items: [
-                  {
-                    text: '架构设计',
-                    link: '../framework/',
-                  }
-                ]
-              }
+                {
+                    text: 'Vue',
+                    collapsible: true,
+                    items: [
+                        {
+                            text: 'vue相关主题',
+                            link: '../vue/',
+                            collapsible: true, 
+                            items: [
+                                {
+                                    text: 'vue2和vue3核心原理差异',
+                                    link: '../vue/core'
+                                }    
+                            ]
+                        }
+                    ]
+                },
+                {
+                    text: 'React',
+                    collapsible: true,
+                    items: [
+                        {
+                            text: 'React核心',
+                            link: '../react/',
+                        }
+                    ]
+                },
+                {
+                    text: '前端架构',
+                    collapsible: true,
+                    items: [
+                        {
+                            text: '架构设计',
+                            link: '../framework/',
+                        }
+                    ]
+                }
             ]
         }
     }
