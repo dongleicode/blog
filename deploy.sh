5262c93e32e9fecd43f2de42cc500e9eee659c6e
dongleicode <mhwgodl@163.com>
2023-01-29 15:26:57 +0800

    修改部署文件

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,9 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git branch 
 # git@github.com:dongleicode/dongleicode.github.io.git
-# git push -f git@github.com:dongleicode/dongleicode.github.io.git dev:main
+git push -f git@github.com:dongleicode/dongleicode.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>dongleicode
-git push -f git@github.com:dongleicode/blog.git pro
+# git push -f git@github.com:dongleicode/blog.git pro
 
 
